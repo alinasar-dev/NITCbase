009c1d5be1e19a4690dfc42ae39078b9fc60ff8c
Ali Nasar <www.alinasar@gmail.com>
2026-08-06 04:15:01 +0530

    Stage 2 (Exercise: Q1) -- Read across multiple blocks of the attribute catalog

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -11,7 +11,6 @@ int main(int argc, char *argv[]) {
 
   // create objects for the relation catalog and attribute catalog
   RecBuffer relCatBuffer(RELCAT_BLOCK);
-  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
   HeadInfo relCatHeader;
   HeadInfo attrCatHeader;
@@ -19,7 +18,6 @@ int main(int argc, char *argv[]) {
   // load the headers of both the blocks into relCatHeader and attrCatHeader.
   // (we will implement these functions later)
   relCatBuffer.getHeader(&relCatHeader);
-  attrCatBuffer.getHeader(&attrCatHeader);
 
   for (int i = 0; i < relCatHeader.numEntries; i++) {
 
@@ -28,20 +26,29 @@ int main(int argc, char *argv[]) {
 
     printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
 
-    for (int j = 0; j < attrCatHeader.numEntries; j++) {
+    
+    // UPDATED CODE FOR STAGE 2 EXERCISE: (read across multiple blocks of the attribute catalog)
+    int currentBlock = ATTRCAT_BLOCK;
+    
+    while (currentBlock != INVALID_BLOCKNUM)
+    {
+        RecBuffer attrCatBuffer(currentBlock);
+        HeadInfo attrCatHeader;
 
-      // declare attrCatRecord and load the attribute catalog entry into it
-      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-      attrCatBuffer.getRecord(attrCatRecord, j);
+        attrCatBuffer.getHeader(&attrCatHeader);
 
-      // Check attribute catalog entry corresponds to the current relation
-      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,
-                 relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) {
+        for (int j = 0; j < attrCatHeader.numEntries; j++) {
+            Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+            attrCatBuffer.getRecord(attrCatRecord, j);
 
-        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
-
-        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
-      }
+            if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,
+                    relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) 
+            {
+                const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+                printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+            }
+        }
+        currentBlock = attrCatHeader.rblock;
     }
     printf("\n");
   }
