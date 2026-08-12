1577ff9f147371c4d512476287b2aa2215838176
Ali Nasar <www.alinasar@gmail.com>
2026-08-12 22:00:57 +0530

    Stage 3: The Disk Buffer

--- NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -11,26 +11,31 @@ BlockBuffer::BlockBuffer(int blockNum) {
 // calls the parent class constructor
 RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
 
-
-// load the block header into the argument pointer
+/*
+Used to get the header of the block into the location pointed to by `head`
+NOTE: this function expects the caller to allocate memory for `head`
+*/
 int BlockBuffer::getHeader(struct HeadInfo *head) {
-  unsigned char buffer[BLOCK_SIZE];
-
-  // read the block at this.blockNum into the buffer
-  Disk::readBlock(buffer, this->blockNum);
+  unsigned char *bufferPtr;
+  int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+  if (ret != SUCCESS) {
+    return ret;
+  }
 
   // populate the numEntries, numAttrs and numSlots fields in *head
-  memcpy(&head->numSlots, buffer + 24, 4);
-  memcpy(&head->numEntries, buffer + 16, 4);
-  memcpy(&head->numAttrs, buffer + 20, 4);
-  memcpy(&head->rblock, buffer + 12, 4);
-  memcpy(&head->lblock, buffer + 8, 4);
+  memcpy(&head->lblock, bufferPtr + 8, 4);
+  memcpy(&head->rblock, bufferPtr + 12, 4);
+  memcpy(&head->numEntries, bufferPtr + 16, 4);
+  memcpy(&head->numAttrs, bufferPtr + 20, 4);
+  memcpy(&head->numSlots, bufferPtr + 24, 4);
 
   return SUCCESS;
 }
 
-
-// load the record at slotNum into the argument pointer
+/*
+Used to get the record at slot `slotNum` into the array `rec`
+NOTE: this function expects the caller to allocate memory for `rec`
+*/
 int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
   struct HeadInfo head;
 
@@ -40,19 +45,18 @@ int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
   int attrCount = head.numAttrs;
   int slotCount = head.numSlots;
 
-  unsigned char buffer[BLOCK_SIZE];
-
-  // read the block at this.blockNum into a buffer
-  Disk::readBlock(buffer, this->blockNum);
+  unsigned char *bufferPtr;
+  int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+  if (ret != SUCCESS) {
+    return ret;
+  }
 
   /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
      - each record will have size attrCount * ATTR_SIZE
      - slotMap will be of size slotCount
   */
   int recordSize = attrCount * ATTR_SIZE;
-
-  unsigned char *slotPointer =
-      buffer + HEADER_SIZE + slotCount + (recordSize * slotNum);
+  unsigned char *slotPointer = bufferPtr + HEADER_SIZE + slotCount + (recordSize * slotNum);
 
   // load the record into the rec data structure
   memcpy(rec, slotPointer, recordSize);
@@ -67,15 +71,40 @@ int RecBuffer::setRecord(union Attribute *rec, int slotNum)
     HeadInfo head;
     this->getHeader(&head);
 
-    unsigned char buffer[BLOCK_SIZE];
-    Disk::readBlock(buffer, this->blockNum);
+    unsigned char *bufferPtr;
+    int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+    if (ret != SUCCESS) {
+      return ret;
+    }
 
     int recordSize = head.numAttrs * ATTR_SIZE;
-
-    unsigned char *slotPointer = buffer + HEADER_SIZE + head.numSlots + (recordSize * slotNum);
+    unsigned char *slotPointer = bufferPtr + HEADER_SIZE + head.numSlots + (recordSize * slotNum);
 
     memcpy(slotPointer, rec, recordSize);
 
-    Disk::writeBlock(buffer, this->blockNum);
+    Disk::writeBlock(bufferPtr, this->blockNum);
     return SUCCESS;
+}
+
+/*
+Used to load a block to the buffer and get a pointer to it.
+NOTE: this function expects the caller to allocate memory for the argument
+*/
+int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char **bufferPtr) {
+  // check whether the block is already present in the buffer using StaticBuffer.getBufferNum()
+  int bufferNum = StaticBuffer::getBufferNum(this->blockNum);
+
+  if (bufferNum == E_BLOCKNOTINBUFFER) {
+    bufferNum = StaticBuffer::getFreeBuffer(this->blockNum);
+
+    if (bufferNum == E_OUTOFBOUND) {
+      return E_OUTOFBOUND;
+    }
+    Disk::readBlock(StaticBuffer::blocks[bufferNum], this->blockNum);
+  }
+
+  // store the pointer to this buffer (blocks[bufferNum]) in *buffPtr
+  *bufferPtr = StaticBuffer::blocks[bufferNum];
+  
+  return SUCCESS;
 }
--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+using namespace std;
 
 #include "Disk_Class/Disk.h"
 #include "define/constants.h"
@@ -8,58 +9,18 @@
 
 int main(int argc, char *argv[]) {
   Disk disk_run;
+  StaticBuffer buffer;
 
-  // create objects for the relation catalog and attribute catalog
-  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  BlockBuffer blockBuffer(700);
+  HeadInfo head;
 
-  HeadInfo relCatHeader;
-  HeadInfo attrCatHeader;
-
-  // load the headers of both the blocks into relCatHeader and attrCatHeader.
-  // (we will implement these functions later)
-  relCatBuffer.getHeader(&relCatHeader);
-
-  for (int i = 0; i < relCatHeader.numEntries; i++) {
-
-    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
-    relCatBuffer.getRecord(relCatRecord, i);
-
-    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
-
-    
-    // UPDATED CODE FOR STAGE 2 EXERCISE: (read across multiple blocks of the attribute catalog)
-    int currentBlock = ATTRCAT_BLOCK;
-    
-    while (currentBlock != INVALID_BLOCKNUM)
-    {
-        RecBuffer attrCatBuffer(currentBlock);
-        HeadInfo attrCatHeader;
-
-        attrCatBuffer.getHeader(&attrCatHeader);
-
-        for (int j = 0; j < attrCatHeader.numEntries; j++) {
-            Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-            attrCatBuffer.getRecord(attrCatRecord, j);
-
-            if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,
-                    relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) 
-            {
-                // Modification for STAGE 2 Exercise Q2: 
-                // In "Students" relation, Change the attribute name "Class" to "Batch"
-                if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, "Students") == 0 &&
-                    strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Class") == 0)
-                {
-                    strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Batch");
-                    attrCatBuffer.setRecord(attrCatRecord, j);
-                }
-
-                const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
-                printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
-            }
-        }
-        currentBlock = attrCatHeader.rblock;
-    }
-    printf("\n");
+  // First access
+  int ret = blockBuffer.getHeader(&head);
+  if (ret != SUCCESS) {
+    cout << "Block access failed\n";
+    return ret;
   }
+  cout << "Block loaded successfully\n";
+
   return 0;
 }
